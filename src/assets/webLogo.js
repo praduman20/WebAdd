bd0fbf33427f7592884637081744634d152947ac
praduman20 <pschundawat4080@gmail.com>
2021-12-06 00:03:59 +0530

    Initial commit

---
+++ src/assets/webLogo.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default  function OlxLogo() {
+    return(  
+          <svg
+        width="48px"
+        height="48px"
+        viewBox="0 0 1024 1024"
+        data-aut-id="icon"
+        fillRule="evenodd"
+      >
+        <path
+          className="rui-77aaa"
+          d="M34530 34689 c0 -5 5 -7 10 -4 6 3 10 8 10 11 0 2 -4 4 -10 4 -5 0
+          -10 -5 -10 -11z"
+        />
+      </svg>)
+}
